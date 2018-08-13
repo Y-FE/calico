ce219d9d373dbbd3ca28fc4254101c8f8455a676
secretmm <1584949586@qq.com>
2018-08-13 13:51:52 +0800

    message

---
+++ packages/message/index.js
@@ -0,0 +1,25 @@
+import CcMessage from './src/message';
+const MessageConstructor = Vue.extend(CcMessage);
+let MessageInstance;
+let Message = (content) => {
+    MessageInstance = new MessageConstructor({
+        data: {
+            message: content
+        }
+    }) 
+    MessageInstance.$mount(); 
+    MessageInstance.dom = MessageInstance.$el;
+    document.body.appendChild(MessageInstance.dom);
+    return MessageInstance;
+}
+
+export default {
+    install: Vue => {
+        Vue.prototype.$message = Message; 
+    }
+}
+// CcMessage.install = function(Vue) {
+//     Vue.component(CcMessage.name, CcMessage);
+// };
+    
+// export default CcMessage;
---
+++ packages/message/src/message.vue
@@ -0,0 +1,58 @@
+<template>
+    <transition>
+        <div v-if="show" class="cat-message-body cat-layout-flex--align">
+            <p></p>
+        </div>
+    </transition>
+</template>
+<style lang="scss">
+    .cat-message-body {
+        color: $--message-color;
+        font-size: $--message-font-size;
+        background-color: $--message-fill;
+        border-radius: $--message-radius;
+        padding: $--message-padding;
+        // @include cat-layout-flex--justify(space-between);
+        // @include cat-layout-flex--aligin;
+    }
+</style>
+<script>
+    export default {
+        name: 'CcMessage',
+        components: {
+        },
+        props: {},
+        data() {
+            return {
+                show: false,
+                message: null,
+            }
+        },
+        computed: {
+        },
+        watch: {
+        },
+        methods: {
+            startTime(){
+                this.show = true;
+                // setTimeout(() => {
+                //     this.close();
+                // }, 3000);
+            },
+            close() {
+                this.show = false;
+                setTimeout(() => {
+                    this.$destroy(true);
+                    this.$el.parentNode.removeChild(this.$el) // 从DOM里将这个组件移除
+                }, 500);
+            }
+        },
+        created() {
+        },
+        destroyed() {
+        },
+        mounted() {
+            this.startTime();
+        }
+    }
+</script>
