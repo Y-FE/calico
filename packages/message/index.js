4ac5ea5e46961396e334594ad44dcec1c92ac4a3
pxqing <panxiuqing@gmail.com>
2018-08-15 15:41:17 +0800

    import Vue in message

--- packages/message/index.js
+++ packages/message/index.js
@@ -1,25 +1,27 @@
 import CcMessage from './src/message';
+import Vue from 'vue';
+
 const MessageConstructor = Vue.extend(CcMessage);
 let MessageInstance;
-let Message = (content) => {
+let Message = content => {
     MessageInstance = new MessageConstructor({
         data: {
             message: content
         }
-    }) 
-    MessageInstance.$mount(); 
+    });
+    MessageInstance.$mount();
     MessageInstance.dom = MessageInstance.$el;
     document.body.appendChild(MessageInstance.dom);
     return MessageInstance;
-}
+};
 
 export default {
     install: Vue => {
-        Vue.prototype.$message = Message; 
+        Vue.prototype.$message = Message;
     }
-}
+};
 // CcMessage.install = function(Vue) {
 //     Vue.component(CcMessage.name, CcMessage);
 // };
-    
+
 // export default CcMessage;
