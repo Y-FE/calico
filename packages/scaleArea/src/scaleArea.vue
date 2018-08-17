9158571fe8fe1662142a6edf06e1a5e8c48be288
Q1FRJ0S13Z733OG\Administrator <zhangyi@yi-you.org>
2018-08-17 17:57:59 +0800

    增加scaleArea

--- packages/scaleArea/src/scaleArea.vue
+++ packages/scaleArea/src/scaleArea.vue
@@ -1,15 +1,36 @@
 <template>
+    <div class="cat-scale-area" 
+        :style="[{'width': parseInt(myWidth) + 'px'},
+            {'height': parseInt(myHeight) + 'px'}]">
+    </div>
 </template>
 <style lang="scss">
+    .cat-scale-area {
+        border: 1px solid #403bef;
+      
+    }
 </style>
 <script>
     export default {
         name: 'CcScaleArea',
         components: {
         },
-        props: {},
+        props: {
+            width: {
+                type: Number,
+                default: 1336
+            },
+            height: {
+                type: Number,
+                default: 768
+            }
+        },
         data() {
             return {
+                calBgWidth: '',
+                calBgHeight: '',
+                myWidth: this.width,
+                myHeight: this.height
             }
         },
         computed: {
@@ -23,6 +44,14 @@
         destroyed() {
         },
         mounted() {
+            // 然后监听window的resize事件．在浏览器窗口变化时再设置下背景图高度．
+            const that = this;
+            that.calBgWidth = document.body.clientWidth / that.width;
+            that.calBgHeight = document.documentElement.clientHeight / that.height;
+            window.onresize = function temp() {
+                that.myWidth = document.body.clientWidth / that.calBgWidth;
+                that.myHeight = document.documentElement.clientHeight / that.calBgHeight;
+            }
         }
     }
 </script>
