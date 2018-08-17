ad83218d53829ba0e5ac2545716d4c58c347f279
secretmm <1584949586@qq.com>
2018-08-17 10:08:22 +0800

    popover提交

--- packages/popover/src/popover.vue
+++ packages/popover/src/popover.vue
@@ -1,6 +1,17 @@
 <template>
+    <div class="cat-popover-body">
+
+    </div>
 </template>
 <style lang="scss">
+    .cat-popover-body {
+        background-color:  $--color-white;
+        box-shadow: $--popover-box-shadow;
+        padding: $--popover-padding;
+        color: $--popover-color;
+        font-size: $--popover-font-size;
+        border-radius: $--popover-radius;
+    }
 </style>
 <script>
     export default {
