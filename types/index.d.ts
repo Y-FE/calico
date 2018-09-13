58604b451be2b515eb210f8a5c2918183a094d7d
secretmm <1584949586@qq.com>
2018-09-13 11:32:44 +0800

    加了一个default

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,3 @@
 import Vue, { VueConstructor } from "vue";
 
-export function install(vue: VueConstructor<Vue>, options: any): void;
+export default function install(vue: VueConstructor<Vue>, options: any): void;
